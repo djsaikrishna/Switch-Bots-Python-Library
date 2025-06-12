761f27cf4705187482f1b8e4bc8fa48b5ddda6f2
New-dev0 <newdev0@outlook.com>
2025-06-12 18:58:50 +0530

    fix: Update base domain

--- swibots/config.py
+++ swibots/config.py
@@ -3,26 +3,26 @@ import os
 
 APP_CONFIG = {
     "CHAT_SERVICE": {
-        "BASE_URL": os.getenv("CHAT_SERVICE_BASE_URL") or "https://chat-api.switch.pe",
+        "BASE_URL": os.getenv("CHAT_SERVICE_BASE_URL") or "https://chat-api.switchx.org",
         "WS_URL": os.getenv("CHAT_SERVICE_WS_URL")
-        or "wss://chat-api.switch.pe/v1/websocket/message/ws",
+        or "wss://chat-api.switchx.org/v1/websocket/message/ws",
     },
     "BOT_SERVICE": {
-        "BASE_URL": os.getenv("BOT_SERVICE_BASE_URL") or "https://chat-api.switch.pe",
+        "BASE_URL": os.getenv("BOT_SERVICE_BASE_URL") or "https://chat-api.switchx.org",
     },
     "AUTH_SERVICE": {
         "BASE_URL": os.getenv("AUTH_SERVICE_BASE_URL")
-        or "https://gateway.switch.pe/user-service",
+        or "https://gateway.switchx.org/user-service",
     },
     "AIRDROP_SERVICE": {
         "BASE_URL": os.getenv("AIRDROP_SERVICE_BASE_URL")
-        or "https://gateway.switch.pe/airdrop-service"
+        or "https://gateway.switchx.org/airdrop-service"
     },
     "COMMUNITY_SERVICE": {
         "BASE_URL": os.getenv("COMMUNITY_SERVICE_BASE_URL")
-        or "https://gateway.switch.pe/community-service",
+        or "https://gateway.switchx.org/community-service",
         "WS_URL": os.getenv("COMMUNITY_SERVICE_WS_URL")
-        or "wss://gateway.switch.pe/community-service/v1/websocket/community/ws",
+        or "wss://gateway.switchx.org/community-service/v1/websocket/community/ws",
     },
     "BACKBLAZE": {
         "BUCKET_ID": os.getenv("BACKBLAZE_BUCKET_ID"),
@@ -38,29 +38,29 @@ def get_config():
 
 def reload_config():
     APP_CONFIG["CHAT_SERVICE"]["BASE_URL"] = (
-        os.getenv("CHAT_SERVICE_BASE_URL") or "https://chat-api.switch.pe"
+        os.getenv("CHAT_SERVICE_BASE_URL") or "https://chat-api.switchx.org"
     )
     APP_CONFIG["CHAT_SERVICE"]["WS_URL"] = (
         os.getenv("CHAT_SERVICE_WS_URL")
-        or "wss://chat-api.switch.pe/v1/websocket/message/ws"
+        or "wss://chat-api.switchx.org/v1/websocket/message/ws"
     )
     APP_CONFIG["BOT_SERVICE"]["BASE_URL"] = (
-        os.getenv("BOT_SERVICE_BASE_URL") or "https://chat-api.switch.pe"
+        os.getenv("BOT_SERVICE_BASE_URL") or "https://chat-api.switchx.org"
     )
     APP_CONFIG["AUTH_SERVICE"]["BASE_URL"] = (
-        os.getenv("AUTH_SERVICE_BASE_URL") or "https://gateway.switch.pe/user-service"
+        os.getenv("AUTH_SERVICE_BASE_URL") or "https://gateway.switchx.org/user-service"
     )
     APP_CONFIG["COMMUNITY_SERVICE"]["BASE_URL"] = (
         os.getenv("COMMUNITY_SERVICE_BASE_URL")
-        or "https://gateway.switch.pe/community-service"
+        or "https://gateway.switchx.org/community-service"
     )
     APP_CONFIG["COMMUNITY_SERVICE"]["WS_URL"] = (
         os.getenv("COMMUNITY_SERVICE_WS_URL")
-        or "wss://gateway.switch.pe/community-service/v1/websocket/community/ws"
+        or "wss://gateway.switchx.org/community-service/v1/websocket/community/ws"
     )
     APP_CONFIG["AIRDROP_SERVICE"]["BASE_URL"] = (
         os.getenv("AIRDROP_SERVICE_BASE_URL")
-        or "https://gateway.switch.pe/airdrop-service"
+        or "https://gateway.switchx.org/airdrop-service"
     )
 
 
